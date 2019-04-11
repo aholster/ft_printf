979d018babfa68a85028de294c867728773e717b
aholster <aholster@student.codam.nl>
2019-04-11 20:10:52 +0200

    Project initialized
    basic makefile and files added, libft will be added shortly

---
+++ ft_printf.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/01 16:56:14 by aholster       #+#    #+#                */
+/*   Updated: 2019/04/11 19:42:10 by aholster      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+int	ft_printf(char *format, ...)
+{
+	va_list ap;
+	char	*str;
+	t_list	*lst;
+
+	va_start(ap, count);
+
+
+	va_end(ap);
+}
---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/03/01 16:56:13 by aholster       #+#    #+#                */
+/*   Updated: 2019/04/11 19:42:22 by aholster      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+# include <stdarg.h>
+# include "libft.h"
+
+# include <stdlib.h>
+
+int	ft_printf(char *format, ...);
+
+#endif

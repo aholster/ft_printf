aa3d9dedc5b6f6aa38ccc962274d08329a8202e8
Jessica Smith <jesmith@f0r2s16.codam.nl>
2019-06-03 11:33:38 +0200

    Cleaned up ft_str.c
    simplied ft_str and removed all uses of malloc
    
    implimented ft_padder.c

--- writers/ft_str.c
+++ writers/ft_str.c
@@ -6,14 +6,13 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/24 21:38:26 by jesmith        #+#    #+#                */
-/*   Updated: 2019/05/31 19:20:33 by jesmith       ########   odam.nl         */
+/*   Updated: 2019/06/03 10:28:53 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
-static int				flagverif(const unsigned char c, const t_flag *flags)
+static int		flagverif(const unsigned char c, const t_flag *flags)
 {
 	unsigned short flip;
 
@@ -24,40 +23,41 @@ static int				flagverif(const unsigned char c, const t_flag *flags)
 	return (-1);
 }
 
-static int				ft_str_pad(unsigned char *str, size_t len, t_print *clipb)
+static int		ft_str_pad(unsigned char *str, t_print *clipb)
 {
 	if (flagverif('-', clipb->flags) == -1)
 	{
-		if (pad_spaces(clipb->flags->padding - clipb->flags->precision, clipb) == -1)
+		if (pad_spaces(clipb->flags->padding - clipb->flags->precision,\
+		clipb) == -1)
 			return (-1);
 	}
-	len = ft_strlen((const char*)str);
 	clipb->printer(str, (size_t)clipb->flags->precision, clipb);
 	if (flagverif('-', clipb->flags) == 1)
 	{
-		if (pad_spaces(clipb->flags->padding - clipb->flags->precision, clipb) == -1)
+		if (pad_spaces(clipb->flags->padding - clipb->flags->precision,\
+		clipb) == -1)
 			return (-1);
 	}
 	return (1);
 }
 
-static int				ft_str_prec(unsigned char *str, t_print *clipb)
+static int		ft_str_prec(unsigned char *str, t_print *clipb)
 {
 	size_t			len;
 
 	len = ft_strlen((char const*)str);
-	if (len > (size_t)clipb->flags->precision)	 				// string will not be longer that precision
+	if (len > (size_t)clipb->flags->precision)
 		len = (size_t)clipb->flags->precision;
-	if (len < (size_t)clipb->flags->precision)					// precision is invalid if shorter than str
+	if (len < (size_t)clipb->flags->precision)
 		clipb->flags->precision = len;
-	if (clipb->flags->padding > clipb->flags->precision) 		// padding will not be longer than string
-		ft_str_pad(str, len, clipb);
-	if (clipb->flags->padding < clipb->flags->precision) 	
+	if (clipb->flags->padding > clipb->flags->precision)
+		ft_str_pad(str, clipb);
+	if (clipb->flags->padding < clipb->flags->precision)
 		clipb->printer(str, len, clipb);
 	return (1);
 }
 
-int					ft_str(va_list ap, t_print *clipb)
+int				ft_str(va_list ap, t_print *clipb)
 {
 	unsigned char	*str;
 	size_t			len;
@@ -68,8 +68,8 @@ int					ft_str(va_list ap, t_print *clipb)
 	if (flagverif('.', clipb->flags) == 1)
 		return (ft_str_prec(str, clipb));
 	len = ft_strlen((char const*)str);
-	if (flagverif('.', clipb->flags) == -1)    // && ((clipb->flags->padding - (unsigned int)len) > 0))
-		return (ft_str_pad(str, len, clipb));
+	if (flagverif('.', clipb->flags) == -1)
+		return (ft_str_pad(str, clipb));
 	clipb->printer(str, len, clipb);
 	return (1);
 }

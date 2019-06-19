377343d827792286044438d0e18be7da720d2bde
Jessica Smith <jesmith@f1r2s13.codam.nl>
2019-06-19 19:59:28 +0200

    exceptions handler
    ft_exceptions is now a thing

--- ft_dispatcher.c
+++ ft_dispatcher.c
@@ -6,12 +6,12 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/18 16:00:54 by aholster       #+#    #+#                */
-/*   Updated: 2019/06/19 16:28:19 by jesmith       ########   odam.nl         */
+/*   Updated: 2019/06/19 19:56:49 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
+#include <stdio.h>
 
 static int	ft_operator(char conversion)
 {
@@ -20,31 +20,42 @@ static int	ft_operator(char conversion)
 	return (-1);
 }
 
-// static int	ft_expection(unsigned char *specifer, va_list args, t_print clipb)
-// {
-// 	int	index;
-
-// 	index = 0;
-// 	if (specifer[index] == '%')
-// 	{
-// 		if (clipb->printer(specifer, 1, clipb) == -1)
-// 			return (-1);
-// 	}
-// 	return (1);
-// }
+static int	ft_exception(const unsigned char *specifier, t_print *clipb)
+{
+	if (specifier[0] == '%')
+	{
+		if (flagverif('-', clipb->flags) == -1)
+		{
+			if (flagverif('0', clipb->flags) == 1)
+				if (ft_zero_padder(1, clipb) == -1)
+					return (-1);
+			else if (ft_space_padder(1, clipb) == -1)
+				return (-1);
+		}
+		if (clipb->printer(specifier, 1, clipb) == -1)
+			return (-1);
+		if (flagverif('-', clipb->flags) == 1)
+		{
+			if (flagverif('0', clipb->flags) == 1)
+				if (ft_zero_padder(1, clipb) == -1)
+					return (-1);
+			else if (ft_space_padder(1, clipb) == -1)
+				return (-1);
+		}
+	}
+	return (1);
+}
 
 int			ft_dispatcher(const unsigned char *specifier,\
 				t_writer *functbl, t_print *clipb)
 {
-	// int				index;
-	// va_list			args;
+	int				index;
 
-	// va_copy(clipb->args, args);
 	index = ft_operator(specifier[0]);
 	if (index == -1 || functbl[index] == NULL)
 	{
-		// if (ft_excpetion(int)(specifer[0]), args, clipb) == -1)
-		// 	return (-1);
+		if (ft_exception(&specifier[0], clipb) == -1)
+			return (-1);
 	}
 	else
 	{

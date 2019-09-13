9eb9cea8ad4bdf1c87ba3cb2e37ba9d276b491d8
Jessica Smith <jesmith@f1r2s13.codam.nl>
2019-09-13 18:12:33 +0200

    writers/lowhexpoint

--- writers/ft_lowhexpoint.c
+++ writers/ft_lowhexpoint.c
@@ -6,24 +6,23 @@
 /*   By: jesmith <jesmith@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/28 14:42:40 by jesmith        #+#    #+#                */
-/*   Updated: 2019/09/11 10:41:02 by jesmith       ########   odam.nl         */
+/*   Updated: 2019/09/13 17:12:55 by jesmith       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int					exponentbuffer(t_print *clipb, short expon)
+static int					ft_exponbuff(unsigned char *buffer, short expon)
 {
-	unsigned char	buffer[8];
-	size_t			index;
+	unsigned short	index;
 	int				neg;
-	size_t			backup_index;
+	unsigned short	backup_index;
 
 	neg = expon;
-	index = (size_t)ft_nbrlen((long long)expon, 10);
+	index = ft_nbrlen((long long)expon, 10);
 	if (expon < 0)
 		expon *= -1;
-	else 
+	else
 		index++;
 	backup_index = index;
 	while (index > 1)
@@ -37,25 +36,27 @@ static int					exponentbuffer(t_print *clipb, short expon)
 	else
 		buffer[1] = '+';
 	buffer[0] = 'p';
-	if (clipb->printer(buffer, backup_index + 1, clipb) == -1)
-		return (-1);
-	return (1);
+	return (backup_index + 1);
 }
 
 static int					ft_end_pad(t_print *clipb, short expon, \
-						 unsigned short nb_len, unsigned short str_len)
+						unsigned short nb_len, unsigned short str_len)
 {
+	unsigned short	expon_len;
+	unsigned char	buffer[8];
+
 	if (flagverif('.', clipb->flags) == 1 && \
 	clipb->flags->precision != (str_len - 2))
 		if (pad_zero(clipb->flags->precision, clipb) == -1)
 			return (-1);
-	if (exponentbuffer(clipb, expon) == -1)
+	expon_len = ft_exponbuff(buffer, expon);
+	if (clipb->printer(buffer, expon_len, clipb) == -1)
 		return (-1);
 	if (clipb->flags->precision == 0)
 		str_len--;
 	if (flagverif('-', clipb->flags) == 1 \
 	&& clipb->flags->padding > clipb->flags->precision)
-		if (ft_float_pad(nb_len + str_len, str_len, clipb) == -1)
+		if (ft_float_padder(nb_len + str_len, str_len, clipb) == -1)
 			return (-1);
 	return (1);
 }
@@ -76,15 +77,12 @@ static int					ft_front_pad(unsigned char *buffer, \
 		nb_len--;
 	if (flagverif('-', clipb->flags) == -1 && \
 	clipb->flags->padding > clipb->flags->precision)
-		if (ft_float_pad(nb_len + str_len, str_len, clipb) == -1)
+		if (ft_float_padder(nb_len + str_len, str_len, clipb) == -1)
 			return (-1);
 	if (ft_prefix(neg, clipb) == -1)
 		return (-1);
 	if (clipb->printer((const unsigned char *)"0x", 2, clipb) == -1)
 		return (-1);
-	// if (flagverif('-', clipb->flags) == -1 && clipb->flags->padding != 0)
-	// 	if (ft_float_pad(nb_len + str_len, str_len, clipb) == -1)
-	// 		return (-1);
 	if (clipb->printer(buffer, str_len, clipb) == -1)
 		return (-1);
 	if (ft_end_pad(clipb, expon, nb_len, str_len) == -1)
@@ -93,13 +91,13 @@ static int					ft_front_pad(unsigned char *buffer, \
 }
 
 static void					ft_man_to_buffer(unsigned long long mantissa, \
-				unsigned char *buffer, t_print *clipb)
+						unsigned char *buffer, t_print *clipb)
 {
 	char				*base;
 	unsigned short		cur_len;
 	size_t				buffer_trim;
 
-	base = "0123456789abcdef";
+	base = "0123456789ABCDEF";
 	buffer_trim = 0;
 	cur_len = ft_ull_len(mantissa, 16);
 	while (mantissa > 16)
@@ -125,7 +123,7 @@ static void					ft_man_to_buffer(unsigned long long mantissa, \
 int							ft_lowhexpoint(va_list args, t_print *clipb)
 {
 	unsigned char		buffer[20];
-	t_float			conversion;
+	t_float				conversion;
 	long double			nb;
 	int					neg;
 	short				expon;
@@ -133,7 +131,7 @@ int							ft_lowhexpoint(va_list args, t_print *clipb)
 	neg = ft_longdouble_conv(args, &nb, clipb->flags);
 	conversion.ld = nb;
 	if (nb == 0.0)
-		ft_exceptions(buffer, nb, &expon, clipb); 
+		ft_float_exceptions(buffer, nb, &expon, clipb);
 	else
 	{
 		expon = (conversion.byte[4] & 0x7FFF) - 16386;
